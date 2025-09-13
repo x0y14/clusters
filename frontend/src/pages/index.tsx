cbfec391e009137d4adf577a054aea201eaa3c68
x0y14 <ya01ma08@gmail.com>
2025-09-13 21:13:31 +0900

    fmt

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "@/index.css";
-import { App } from "@/components/App.tsx";
+import { App } from "@/components/App.tsx"
+import "@/index.css"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
 // biome-ignore lint/style/noNonNullAssertion: a
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
 	</StrictMode>,
-);
+)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "node:path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
 	root: path.resolve(__dirname, "./src/pages"),
 
 	plugins: [react()],
 	// public はプロジェクト直下を使い続ける（デフォルト互換）
-	// publicDir: path.resolve(__dirname, "public"),
+	publicDir: path.resolve(__dirname, "public"),
 	build: {
 		// 出力先もデフォルト互換でプロジェクト直下 dist/
 		outDir: path.resolve(__dirname, "./dist"),
@@ -18,11 +17,5 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
-		// alias: [
-		// 	{
-		// 		find: "@",
-		// 		replacement: fileURLToPath(new URL("./src", import.meta.url)),
-		// 	},
-		// ],
 	},
-});
+})
