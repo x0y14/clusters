af94e93e204ec74936f9a3ea569e67aa1d2b7601
x0y14 <ya01ma08@gmail.com>
2025-09-14 02:42:09 +0900

    Streamを切り出す

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -1,23 +1,4 @@
-import type { PostData } from "@/models/post"
-import { Post } from "./post/Post"
-
-// サンプルポスト
-const posts: PostData[] = [
-	{
-		author: {
-			handle: "asdfg",
-			displayName: "asdfg",
-			iconPath: "asdfg",
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		},
-		text: "吾輩は猫である。名前はまだ無い。どこで生まれたか頓と見當がつかぬ。何ても暗薄いじめじめした所でニャーニャー泣いて居た事丈は記憶して居る。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。",
-		numOfLike: 21,
-		numOfComment: 2,
-		createdAt: new Date(),
-		updatedAt: new Date(),
-	},
-]
+import { StreamDisplay } from "./stream/Stream"
 
 /**
  * Content
@@ -28,11 +9,8 @@ const posts: PostData[] = [
  */
 export function Content(): React.JSX.Element {
 	return (
-		<div className="flex-1 min-h-0 w-full bg-blue-500 overflow-y-auto flex flex-col items-center gap-2 p-2">
-			<Post postData={posts[0]} />
-			<Post postData={posts[0]} />
-			<Post postData={posts[0]} />
-			<Post postData={posts[0]} />
+		<div className="min-h-0 w-full bg-blue-500 overflow-y-auto">
+			<StreamDisplay />
 		</div>
 	)
 }
---
+++ frontend/src/components/stream/Stream.tsx
@@ -0,0 +1,31 @@
+import { Post } from "@/components/post/Post"
+import type { PostData } from "@/models/post"
+
+// サンプルポスト
+const posts: PostData[] = [
+	{
+		author: {
+			handle: "asdfg",
+			displayName: "asdfg",
+			iconPath: "asdfg",
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		},
+		text: "吾輩は猫である。名前はまだ無い。どこで生まれたか頓と見當がつかぬ。何ても暗薄いじめじめした所でニャーニャー泣いて居た事丈は記憶して居る。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。",
+		numOfLike: 21,
+		numOfComment: 2,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+]
+
+export function StreamDisplay(): React.JSX.Element {
+	return (
+		<div className="flex flex-col items-center gap-2 p-2">
+			<Post postData={posts[0]} />
+			<Post postData={posts[0]} />
+			<Post postData={posts[0]} />
+			<Post postData={posts[0]} />
+		</div>
+	)
+}
